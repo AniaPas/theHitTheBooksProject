aac71c16867a14ef24a6ae82603af4c4f28c89de
Ania <an.pasieczna@gmail.com>
2022-12-06 22:41:11 +0100

    new branch

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,8 @@ import { FC } from "react";
 import style from "./Footer.module.scss";
 export const Footer: FC = () => {
   return (
-    <footer>
-      <div className={style.footer}>
-        <span>Developed by A.P.</span>
-      </div>
-      <div className={style.icon}></div>
+    <footer className={style.footer}>
+      <span>Developed by A.P.</span>
     </footer>
   );
 };
