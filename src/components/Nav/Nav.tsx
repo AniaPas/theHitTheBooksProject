27748759ae7317465eab71722adaa88b5769f8af
Ania <an.pasieczna@gmail.com>
2022-12-07 14:18:40 +0100

    lots of style changes and header added

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,27 +1,28 @@
-import { Link } from 'react-router-dom'
-import { FC } from 'react'
-import { navElements } from '../../HelperInterface/Navigation'
-import style from './Nav.module.scss'
+import { Link } from "react-router-dom";
+import { FC } from "react";
+import { navElements } from "../../HelperInterface/Navigation";
+
+import style from "./Nav.module.scss";
 
 interface PropsNav {
-	children?: JSX.Element | JSX.Element[]
-	navElements: navElements[]
+  children?: JSX.Element | JSX.Element[];
+  navElements: navElements[];
 }
 
-export const Nav: FC<PropsNav> = props => {
-	return (
-		<nav>
-			<ul className={style.menu}>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/all">All Books</Link>
-				</li>
-				<li>
-					<Link to="add">Add Books</Link>
-				</li>
-			</ul>
-		</nav>
-	)
-}
+export const Nav: FC<PropsNav> = () => {
+  return (
+    <nav>
+      <ul className={style.menu}>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/all'>All Books</Link>
+        </li>
+        <li>
+          <Link to='add'>Add Books</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
