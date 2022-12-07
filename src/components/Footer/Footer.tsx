39d21bb5918861bce187c29abe017edb90fcc5c3
Ania <an.pasieczna@gmail.com>
2022-12-07 17:45:10 +0100

    adding linkedIn icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import style from "./Footer.module.scss";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export const Footer: FC = () => {
   return (
     <footer className={style.footer}>
-      <p className={style.footerText}>Developed by A.P.</p>
+      <p className={style.footerText}>
+        Developed by A.P <LinkedInIcon />
+      </p>
     </footer>
   );
 };
